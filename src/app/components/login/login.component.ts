f992550bda956ae30fa9be0460d2802fe21aa60d
cookie <mail@vks.info>
2024-06-24 19:15:28 +0200

    login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent implements OnInit {
+  email:string = '';
+  password:string = '';
+
+  ngOnInit(): void {
+      
+  }
+
+  login(){
+    
+  }
+}
